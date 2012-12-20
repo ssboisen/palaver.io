616171e9ab2096e53845b9362568f45c8273140d
Simon Skov Boisen <simon@skov-boisen.dk>
2012-12-20 22:24:10 +0100

    Added angularjs

---
+++ public/js/controllers.js
@@ -0,0 +1,53 @@
+function RoomController($scope){
+	$scope.messages = [ 
+	{
+		user: "Simon Skov Boisen",
+		content: "hello world",
+		date: new Date()
+	},
+	{
+		user: "Simon Skov Boisen",
+		content: "hello world",
+		date: new Date()
+	},
+	{
+		user: "Simon Skov Boisen",
+		content: "hello world",
+		date: new Date()
+	},
+	{
+		user: "Simon Skov Boisen",
+		content: "hello world",
+		date: new Date()
+	},
+	{
+		user: "Simon Skov Boisen",
+		content: "hello world",
+		date: new Date()
+	},
+	{
+		user: "Simon Skov Boisen",
+		content: "hello world",
+		date: new Date()
+	},
+	{
+		user: "Simon Skov Boisen",
+		content: "hello world",
+		date: new Date()
+	},
+	{
+		user: "Simon Skov Boisen",
+		content: "hello world",
+		date: new Date()
+	},
+	{
+		user: "Simon Skov Boisen",
+		content: "hello denmark",
+		date: new Date()
+	}];
+
+	$scope.users = [ 
+	{
+		username: "Simon Skov Boisen"
+	}];
+}
